02997d92893a79883019245212214fe3e8e3c906
Sunryeo <elma9700@gmail.com>
2024-07-25 09:27:53 +0900

    initial commit develop

---
+++ src/test/java/com/example/ormi5projectteam4/Ormi5ProjectTeam4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ormi5projectteam4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ormi5ProjectTeam4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
