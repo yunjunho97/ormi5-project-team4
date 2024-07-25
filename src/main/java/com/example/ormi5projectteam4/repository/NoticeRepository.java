7b83697f5f799d2ac5a7195f273478bfe28a43f8
The Shinee <teraburst@gmail.com>
2024-07-25 17:08:33 +0900

    🔥NoticeRepository 파일 제거 작업 영역이 겹쳐 Conflict 발생 우려가 있는 파일 제거

--- src/main/java/com/example/ormi5projectteam4/repository/NoticeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.ormi5projectteam4.repository;
-
-import com.example.ormi5projectteam4.domain.Notice;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NoticeRepository extends JpaRepository<Notice, Integer>{
-
-}
