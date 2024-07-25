867f84ebd6c3ce3ed0e69ef36a61747872324d98
Sunryeo <elma9700@gmail.com>
2024-07-25 10:29:04 +0900

    feat: create User entity

---
+++ src/main/java/com/example/ormi5projectteam4/domain/Role.java
@@ -0,0 +1,5 @@
+package com.example.ormi5projectteam4.domain;
+
+public enum Role {
+    ADMIN, ACTIVE, INACTIVE
+}
---
+++ src/main/java/com/example/ormi5projectteam4/domain/User.java
@@ -0,0 +1,43 @@
+package com.example.ormi5projectteam4.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_name")
+    private String userName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "password_answer")
+    private String passwordAnswer;
+
+    @Column(name = "phone")
+    private String phone;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdAt;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updatedAt;
+}
