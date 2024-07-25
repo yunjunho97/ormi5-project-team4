e1c1cf345b5d4891542448508574f1050f5fcab4
juungmiin <ajmkk7@gmail.com>
2024-07-25 13:48:15 +0900

    Creating a Notice Entity

---
+++ src/main/java/com/example/domain/Notice.java
@@ -0,0 +1,35 @@
+package com.example.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Notice {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @ManyToOne
+    private User userId;
+
+}
