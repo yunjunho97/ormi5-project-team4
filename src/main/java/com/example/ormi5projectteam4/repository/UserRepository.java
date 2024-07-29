cd9e06eb8e49594ec0850772fc98d158e0c2ae5c
The Shinee <teraburst@gmail.com>
2024-07-29 15:09:36 +0900

    ✨UserRepository - 유저 이메일 검색 Method 추가, Generic Type Long으로 변경
    두 가지 기능입니다
    - Admin 관점에서 유저의 Email을 검색하기 위한 Method 추가
    - User의 Id type이 Long인데 Repository에는 Integer로 되어 있어 이 부분을 고침

--- src/main/java/com/example/ormi5projectteam4/repository/UserRepository.java
+++ src/main/java/com/example/ormi5projectteam4/repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.example.ormi5projectteam4.repository;
 import com.example.ormi5projectteam4.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Integer> {
+import java.util.List;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    List<User> findByEmailContaining(String email);
 }
