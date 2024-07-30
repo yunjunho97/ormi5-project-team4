486b7dd82f2f3e3605aebe59e3078093a6917378
The Shinee <teraburst@gmail.com>
2024-07-30 22:33:07 +0900

    ♻️#41 api 통신을 위한 js 파일 수정 작업내역 중간저장

--- src/main/resources/static/js/admin-page.js
+++ src/main/resources/static/js/admin-page.js
@@ -1,27 +1,28 @@
 const URL = "http://localhost:8080";
 document.addEventListener("DOMContentLoaded", function() {
     const pageId = getPageId();
+    const approveStatus = getApproveStatus();
 
     // 게시글 받아오기
-    fetch(`/admin/post/${pageId}`)
+    fetch(`/admin/post?&page=0`)
         .then(response => response.json())
         .then(data => {
             const postList = document.getElementById('post-list');
-            data.forEach(post => {
+            data.content.forEach(post => {
                 const listItem = document.createElement('li');
-                    listItem.className = 'container-post-img'
-                    const div = document.createElement('div');
-                        div.className = 'image-container'
-                        const input = document.createElement('input');
-                            input.type = 'checkbox';
-                            input.className = 'checkbox';
-                        const img = document.createElement('img');
-                            img.alt = '이미지';
-                    const status = document.createElement("p");
-                        status.textContent = getResponseForAdoptionStatus(post.adoptionStatus);
-                    const title = document.createElement('a');
-                        // todo : title.href = 'http://localhost:8080/'
-                        title.textContent = post.title;
+                listItem.className = 'container-post-img'
+                const div = document.createElement('div');
+                div.className = 'image-container'
+                const input = document.createElement('input');
+                input.type = 'checkbox';
+                input.className = 'checkbox';
+                const img = document.createElement('img');
+                img.alt = '이미지';
+                const status = document.createElement("p");
+                status.textContent = getResponseForAdoptionStatus(post.adoptionStatus);
+                const title = document.createElement('a');
+                // todo : title.href = 'http://localhost:8080/'
+                title.textContent = post.title;
 
                 div.appendChild(input);
                 div.appendChild(img);
@@ -30,28 +31,22 @@ document.addEventListener("DOMContentLoaded", function() {
                 listItem.appendChild(title);
 
                 postList.appendChild(listItem);
-
             });
-        })
-        .catch(error => console.error('Error:', error));
 
-    // 게시글 페이지 개수 생성
-    fetch('/admin/post')
-        .then(response => response.json())
-        .then(data => {
-            const itemCount = data.length;
-            const numOfLists = Math.ceil(itemCount / 6);
+            // 게시글 페이지 개수 생성
+            const itemCount = data.totalElements;
+            const numOfLists = data.totalPages;
             const itemList = document.getElementById('post-pages');
 
             // 이전 페이지 바로가기 이미지
             const previousLi = document.createElement('li');
-                const goPreviousPage = document.createElement('a');
-                    const previousPageImg = document.createElement('img');
-                        previousPageImg.src = '/images/page-previous.svg';
-                        previousPageImg.alt = '이전 페이지'
-                if(!(pageId === '1')){
-                    goPreviousPage.href = URL + `/manage/posts/` + (parseInt(pageId) - 1).toString();
-                }
+            const goPreviousPage = document.createElement('a');
+            const previousPageImg = document.createElement('img');
+            previousPageImg.src = '/images/page-previous.svg';
+            previousPageImg.alt = '이전 페이지'
+            if (!(pageId === '1')) {
+                goPreviousPage.href = URL + `/manage/posts/` + (parseInt(pageId) - 1).toString();
+            }
 
             goPreviousPage.appendChild(previousPageImg);
             previousLi.appendChild(goPreviousPage);
@@ -60,17 +55,16 @@ document.addEventListener("DOMContentLoaded", function() {
             // 페이지 생성
             for (let i = 0; i < numOfLists; i++) {
                 const li = document.createElement('li');
-                    const pageElement = document.createElement(
-                        pageId === (i + 1).toString() ? 'p' : 'a'
-                    )
-                    pageElement.textContent = (i + 1).toString();
-                    if(pageElement.tagName.toLowerCase() === 'p'){
-                        pageElement.className = 'font-page-selected';
-                    }
-                    else{
-                        pageElement.className = 'font-page';
-                        pageElement.href = URL + `/manage/posts/` + (i + 1).toString();
-                    }
+                const pageElement = document.createElement(
+                    pageId === (i + 1).toString() ? 'p' : 'a'
+                )
+                pageElement.textContent = (i + 1).toString();
+                if (pageElement.tagName.toLowerCase() === 'p') {
+                    pageElement.className = 'font-page-selected';
+                } else {
+                    pageElement.className = 'font-page';
+                    pageElement.href = URL + `/manage/posts/` + (i + 1).toString();
+                }
 
                 li.appendChild(pageElement);
                 itemList.appendChild(li);
@@ -78,18 +72,17 @@ document.addEventListener("DOMContentLoaded", function() {
 
             // 다음 페이지 바로가기 이미지
             const nextLi = document.createElement('li');
-                const goNextPage = document.createElement('a');
-                    const nextPageImg = document.createElement('img');
-                        nextPageImg.src = '/images/page-next.svg';
-                        nextPageImg.alt = '다음 페이지'
-                if(!(pageId === numOfLists.toString())){
-                    goNextPage.href = URL + `/manage/posts/` + (parseInt(pageId) + 1).toString();
-                }
+            const goNextPage = document.createElement('a');
+            const nextPageImg = document.createElement('img');
+            nextPageImg.src = '/images/page-next.svg';
+            nextPageImg.alt = '다음 페이지'
+            if (!(pageId === numOfLists.toString())) {
+                goNextPage.href = URL + `/manage/posts/` + (parseInt(pageId) + 1).toString();
+            }
 
             goNextPage.appendChild(nextPageImg);
             nextLi.appendChild(goNextPage);
             itemList.appendChild(goNextPage);
-
         })
         .catch(error => console.error('Error:', error));
 });
@@ -105,7 +98,16 @@ function getResponseForAdoptionStatus(adoptionStatus) {
     }
 }
 
-function getPageId(){
-    const pathSegments = window.location.pathname.split('/');
-    return pathSegments[pathSegments.length - 1];
+function getUrlParameter(name) {
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get(name) || '';
+}
+
+function getPageId() {
+    const pageParam = getUrlParameter('page');
+    return isNaN(pageParam) ? 0 : parseInt(pageParam, 10);
+}
+
+function getApproveStatus() {
+    return getUrlParameter('approveStatus') || '';
 }
