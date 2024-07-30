f8e0a440fa66cd06d2940479654dc643832819de
The Shinee <teraburst@gmail.com>
2024-07-30 15:51:16 +0900

    ⚡️#41 관리자 페이지 Controller 게시글 조회 페이지 내용 추가
    게시글 조회 페이지 연결 엔드포인트를 추가했습니다.

--- src/main/java/com/example/ormi5projectteam4/controller/thymeleaf_controller/AdminPageController.java
+++ src/main/java/com/example/ormi5projectteam4/controller/thymeleaf_controller/AdminPageController.java
@@ -2,16 +2,27 @@ package com.example.ormi5projectteam4.controller.thymeleaf_controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class AdminPageController {
-    @GetMapping("/admin")
+    @GetMapping("/manage")
     public String admin() {
-        return "redirect:/admin/posts";
+        return "redirect:/manage/posts/1";
     }
 
-    @GetMapping("/admin/posts")
-    public String posts(){
+    @GetMapping("/manage/posts")
+    public String adminPosts(){
+        return "redirect:/manage/posts/1";
+    }
+
+    @GetMapping("/manage/posts/{id}")
+    public String adminPostsWithPage(@PathVariable Long id){
         return "admin-posts";
+    }
+
+    @GetMapping("/manage/member")
+    public String adminMembers(){
+        return "admin-members";
     }
 }
