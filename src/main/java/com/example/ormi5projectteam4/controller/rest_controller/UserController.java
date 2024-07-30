f2e6f2633fb68030651a240faf1f8806e79843e4
Sunryeo <elma9700@gmail.com>
2024-07-30 15:06:52 +0900

    fix: change password api bug fix

--- src/main/java/com/example/ormi5projectteam4/controller/rest_controller/UserController.java
+++ src/main/java/com/example/ormi5projectteam4/controller/rest_controller/UserController.java
@@ -1,9 +1,12 @@
 package com.example.ormi5projectteam4.controller.rest_controller;
 
 import com.example.ormi5projectteam4.domain.constant.StatusCode;
+import com.example.ormi5projectteam4.domain.constant.StatusMessage;
 import com.example.ormi5projectteam4.domain.dto.*;
 import com.example.ormi5projectteam4.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.annotations.processing.Find;
+import org.springframework.http.HttpMessage;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -25,30 +28,32 @@ public class UserController {
   }
 
   @GetMapping("/email-duplication")
-  public ResponseEntity<Void> validateDuplicateUserEmail(
-      @RequestParam EmailDuplicationRequestDto emailDuplicationRequestDto) {
-    int result = userService.validateDuplicateEmail(emailDuplicationRequestDto);
-    ResponseEntity<Void> response;
+  public ResponseEntity<String> validateDuplicateUserEmail(@RequestParam String email) {
+    int result = userService.validateDuplicateEmail(email);
+    ResponseEntity<String> response;
 
-    if (result != 0) {
+    if (result == 0) {
       response = new ResponseEntity<>(HttpStatus.valueOf(StatusCode.NO_CONTENT));
     } else {
-      response = new ResponseEntity<>(HttpStatus.valueOf(StatusCode.DUPLICATED_USER));
+      response =
+          new ResponseEntity<>(
+              StatusMessage.DUPLICATED_USER, HttpStatus.valueOf(StatusCode.DUPLICATED_USER));
     }
 
     return response;
   }
 
   @GetMapping("/nickname-duplication")
-  public ResponseEntity<Void> validateDuplicateUserName(
-      @RequestParam UserNameDuplicationRequestDto userNameDuplicationRequestDto) {
-    int result = userService.validateDuplicateUserName(userNameDuplicationRequestDto);
-    ResponseEntity<Void> response;
+  public ResponseEntity<String> validateDuplicateUserName(@RequestParam String userName) {
+    int result = userService.validateDuplicateUserName(userName);
+    ResponseEntity<String> response;
 
-    if (result != 0) {
+    if (result == 0) {
       response = new ResponseEntity<>(HttpStatus.valueOf(StatusCode.NO_CONTENT));
     } else {
-      response = new ResponseEntity<>(HttpStatus.valueOf(StatusCode.DUPLICATED_USER));
+      response =
+          new ResponseEntity<>(
+              StatusMessage.DUPLICATED_USER, HttpStatus.valueOf(StatusCode.DUPLICATED_USER));
     }
 
     return response;
@@ -62,11 +67,19 @@ public class UserController {
   }
 
   @PostMapping("/auth/password-certification")
-  public ResponseEntity<UserDto> findUserForPasswordChange(
+  public ResponseEntity<FindUserForPasswordChangeResponseDto> findUserForPasswordChange(
       @RequestBody PasswordCertificationRequestDto passwordCertificationRequestDto) {
-    UserDto userDto = userService.validateUserForPasswordChange(passwordCertificationRequestDto);
+    FindUserForPasswordChangeResponseDto responseDto;
 
-    return new ResponseEntity<>(userDto, HttpStatus.valueOf(StatusCode.SUCCESS));
+    try {
+      UserDto userDto = userService.validateUserForPasswordChange(passwordCertificationRequestDto);
+      responseDto = new FindUserForPasswordChangeResponseDto(StatusMessage.SUCCESS, userDto);
+    } catch (IllegalArgumentException e) {
+      responseDto = new FindUserForPasswordChangeResponseDto(e.getMessage());
+      return new ResponseEntity<>(responseDto, HttpStatus.valueOf(StatusCode.NOT_FOUND));
+    }
+
+    return new ResponseEntity<>(responseDto, HttpStatus.valueOf(StatusCode.SUCCESS));
   }
 
   @PutMapping("/auth/password")
