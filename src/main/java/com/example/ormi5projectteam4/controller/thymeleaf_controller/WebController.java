162c1ed46fba5e622914abdc59af2fb3cc8235f1
juungmiin <ajmkk7@gmail.com>
2024-08-01 03:35:07 +0900

    RestTemplate 적용

--- src/main/java/com/example/ormi5projectteam4/controller/thymeleaf_controller/WebController.java
+++ src/main/java/com/example/ormi5projectteam4/controller/thymeleaf_controller/WebController.java
@@ -1,43 +1,86 @@
 package com.example.ormi5projectteam4.controller.thymeleaf_controller;
 
 import com.example.ormi5projectteam4.domain.dto.NoticeDto;
-import com.example.ormi5projectteam4.service.NoticeService;
 
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class WebController {
 
-    private final NoticeService noticeService;
-
-    public WebController(NoticeService noticeService) {
-        this.noticeService = noticeService;
-    }
+    private final RestTemplate restTemplate = new RestTemplate();
+    private static final String BASE_URL = "http://localhost:8080";
 
     @GetMapping("/notice-list")
     public String getNotices(@RequestParam(defaultValue = "1") int page, Model model) {
-        List<NoticeDto> noticePage = noticeService.getAllNotices();
-        List<NoticeDto> recentNotices = noticeService.getLatestNotices();
+        String url = BASE_URL + "?page=" + page;
+        ResponseEntity<List<NoticeDto>> response = restTemplate.exchange(
+                url,
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<>() {}
+        );
+        List<NoticeDto> noticePage = response.getBody();
+        List<NoticeDto> recentNotices = noticePage.subList(0, 5);
 
         model.addAttribute("notices", noticePage);
         model.addAttribute("recentNotices", recentNotices);
-        model.addAttribute("page", noticePage);
 
         return "notice-list-user";
     }
 
     @GetMapping("/manage/notice")
     public String getAdminNotices(Model model) {
-        List<NoticeDto> noticePage = noticeService.getAllNotices();
+        String url = BASE_URL;
+        ResponseEntity<List<NoticeDto>> response = restTemplate.exchange(
+                url,
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<>() {}
+        );
+
+        List<NoticeDto> noticePage = response.getBody();
 
         model.addAttribute("notices", noticePage);
         model.addAttribute("page", noticePage);
 
         return "notice-list-admin";
+    }
+
+    @GetMapping("/notice/{id}")
+    public String getNotices(@PathVariable Long id, Model model) {
+        String url = BASE_URL + "/" + id;
+        ResponseEntity<NoticeDto> response = restTemplate.exchange(
+                url,
+                HttpMethod.GET,
+                null,
+                NoticeDto.class
+        );
+
+        Optional<NoticeDto> notice = Optional.ofNullable(response.getBody());
+        String recentNoticesUrl = BASE_URL + "/recent";
+        ResponseEntity<List<NoticeDto>> recentNoticesResponse = restTemplate.exchange(
+                recentNoticesUrl,
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<>() {}
+        );
+
+        List<NoticeDto> recentNotices = recentNoticesResponse.getBody();
+
+        model.addAttribute("notice", notice);
+        model.addAttribute("recentNotices", recentNotices);
+
+        return "notice";
     }
 }
