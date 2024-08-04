f5c5b7f1d5ddeb9057c70107ecf29f3d99c28600
The Shinee <teraburst@gmail.com>
2024-08-05 02:25:24 +0900

    ✨#41 유저 nav 레이아웃, 스타일 작업

--- src/main/resources/static/js/constant.js
+++ src/main/resources/static/js/constant.js
@@ -2,12 +2,17 @@ export const URL = "http://localhost:8080";
 
 // API 영역
 export const API_LOGIN = "/auth/login";
+export const API_LOGOUT = '/auth/logout';
+export const API_NOTICE = '/notice';
 export const API_ADMIN_GET_POSTS = "/admin/post";
 
 // 페이지 영역
 export const HOME = '/home';
 export const MANAGE_POST = '/manage/posts';
 export const READ_POST = '/read-post/'; // pathVariable 이 항상 있음
+export const MY_PAGE = '/this/info';
+export const ADMIN_PAGE = '/manage/posts';
+export const NOTICE_LIST = '/notice-list';
 
 // Parameter 영역
 export const PAGE_ID = getPageId();
