5fa3d68896ddacbaf7c6cca946d533ea5a2bcb8a
juungmiin <ajmkk7@gmail.com>
2024-08-06 16:01:19 +0900

    공지 목록 조회 사용자/관리자 API 연결

--- src/main/java/com/example/ormi5projectteam4/controller/thymeleaf_controller/WebController.java
+++ src/main/java/com/example/ormi5projectteam4/controller/thymeleaf_controller/WebController.java
@@ -1,21 +1,15 @@
 package com.example.ormi5projectteam4.controller.thymeleaf_controller;
 
-import com.example.ormi5projectteam4.annotation.Secured;
-import com.example.ormi5projectteam4.domain.constant.Role;
 import com.example.ormi5projectteam4.domain.dto.NoticeDto;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
